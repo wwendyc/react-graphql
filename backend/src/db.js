7a3e3922250b17980b36afc9e60ee14532eb9a7b
Wendy Chang <wendy.changw@gmail.com>
2018-10-26 23:54:50 -0400

    graphQL server set up

---
+++ backend/src/db.js
@@ -0,0 +1,11 @@
+// Connects to remote prisma db and enables querying with JS
+const { Prisma } = require('prisma-binding')
+
+const db = new Prisma({
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
+  debug: false
+})
+
+module.exports = db
