f8ee2a42070926ea332e27ff06b6faf62954eb70
Wendy Chang <wendy.changw@gmail.com>
2018-11-06 22:53:09 -0500

    single item query

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -12,15 +12,23 @@ const Mutations = {
     )
 
     return item
+  },
+  updateItem(parent, args, ctx, info) {
+    // take a copy of the updates
+    const updates = { ...args }
+    // remove Id from updates
+    delete updates.id
+    // run the update method
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id
+        }
+      },
+      info
+    )
   }
-  // createDog(parent, args, ctx, info) {
-  //   global.dogs = global.dogs || []
-
-  //   // create a dog!
-  //   const newDog = {name: args.name}
-  //   global.dogs.push(newDog)
-  //   return newDog
-  // }
 }
 
 module.exports = Mutations
