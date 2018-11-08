04900b297bf4f5afd2723bb24f9a4149024fedf9
Wendy Chang <wendy.changw@gmail.com>
2018-11-07 23:58:58 -0500

    delete item

--- frontend/components/Item.js
+++ frontend/components/Item.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Link from 'next/link'
+
 import Title from './styles/Title'
 import ItemStyles from './styles/ItemStyles'
 import PriceTag from './styles/PriceTag'
+import DeleteItem from './DeleteItem'
+
 import formatMoney from '../lib/formatMoney'
 
 export default class Item extends Component {
@@ -39,7 +42,7 @@ export default class Item extends Component {
             <a>Edit ✏️</a>
           </Link>
           <button>Add to cart</button>
-          <button>Delete </button>
+          <DeleteItem>Delete</DeleteItem>
         </div>
       </ItemStyles>
     )
