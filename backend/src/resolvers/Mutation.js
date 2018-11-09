f9cec4706edb62e8a669ea530c096e5dd7b0605b
Wendy Chang <wendy.changw@gmail.com>
2018-11-08 23:04:59 -0500

    single item display

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -28,6 +28,15 @@ const Mutations = {
       },
       info
     )
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id }
+    // 1. find the item
+    const item = await ctx.db.query.item({ where }, `{id title}`)
+    // 2. check if they own that item, or have permissions
+    // TODO
+    // 3. delete item!
+    return ctx.db.mutation.deleteItem({ where }, info)
   }
 }
 
