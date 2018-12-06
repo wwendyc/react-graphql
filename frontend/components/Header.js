8e60715c74eab6246a08869f53b9b2ca217a0192
Wendy Chang <wendy.changw@gmail.com>
2018-12-05 23:11:16 -0500

    cart creation

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,8 +1,10 @@
 import Nav from './Nav'
 import Link from 'next/link'
+import NProgress from 'nprogress'
 import styled from 'styled-components'
 import Router from 'next/router'
-import NProgress from 'nprogress'
+
+import Cart from './Cart'
 
 Router.onRouteChangeStart = () => {
   NProgress.start()
@@ -65,7 +67,7 @@ const Header = () => (
     <div className="sub-bar">
       <p>Search</p>
     </div>
-    <div>Cart</div>
+    <Cart />
   </StyledHeader>
 )
 
